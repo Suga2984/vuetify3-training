1f827543bbf1d9b3be250d34207867c4cba519be
suga <ksc.suga@gmail.com>
2023-11-15 00:01:29 +0900

    FIX

--- src/components/organisms/playground/ScrollTest/ScrollTestHeader.vue
+++ src/components/organisms/playground/ScrollTest/ScrollTestHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex">
     <v-toolbar>
-      <v-toolbar-title>PDF Creater</v-toolbar-title>
+      <v-toolbar-title>TITLE</v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
   </div>
